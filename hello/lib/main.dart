01ec0285d770ceb79fedba8e0ffd83fc9d12223d
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2021-05-11 18:38:13 +0500

    First Commit

---
+++ hello/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+void main() {
+  runApp(MyApp());
+  }
+  
+  
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    
+
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: ThemeData(primaryColor: Colors.purple[900]),
+      home: RandomWords());
+  }
+}
+
+
+
+
+
